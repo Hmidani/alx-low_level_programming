bf32b02d1ad72136c1f0f794c8fe1d251cfe64d3
Abdelhamid Hmidani <rub460483@gmail.com>
2023-02-21 17:45:15 -0800

    Even Liber Abbaci task

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,13 +13,11 @@ int main(void)
 
 	while (k + j < 4000000)
 	{
-		k = k + j;
-		if (k % 2 == 0)
-		{
-			sum = sum + k;
-			j = k - j;
-			++a;
-		}
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	j = k - j;
+	++a;
 	}
 	printf("%ld\n", sum);
 	return (0);
