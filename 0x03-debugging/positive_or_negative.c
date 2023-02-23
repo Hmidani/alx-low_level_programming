6277e197861e5608a19c3521b708143c9d0439c9
Abdelhamid Hmidani <rub460483@gmail.com>
2023-02-23 08:30:11 -0800

    Multiple mains task

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -4,25 +4,25 @@
 
 /**
  * positive_or_negative - a function that test if negative, positive or null.
- *@a: value 1
+ *@i: value 1
  *
  * Return: Always 0.
  *On error, -1 is returned, and errno is set appropriately.
  */
 
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
 
-if (n > 0)
+if (i > 0)
 {
-	printf("%d is positive\n", n);
+	printf("%d is positive\n", i);
 }
-else if (n == 0)
+else if (i == 0)
 {
-	printf("%d is zero\n", n);
+	printf("%d is zero\n", i);
 }
 else
 {
-	printf("%d is negative\n", n);
+	printf("%d is negative\n", i);
 }
 }
