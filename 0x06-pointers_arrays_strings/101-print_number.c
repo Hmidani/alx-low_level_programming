1de9a0b63b0095c638342bb18bde94b5dfd7a370
Abdelhamid Hmidani <rub460483@gmail.com>
2023-03-03 13:54:05 -0800

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
- * swap_int - a function that swaps the values of two integers..
- * @a: the first input.
- * @b: the second input.
+ * print_number - a function that prints an integer.
+ * @n: the integer input.
  *
  * Return: 0 always
  */
 
-void swap_int(int *a, int *b)
+void print_number(int n)
 {
-	int c;
+	unsigned int c = n;
 
-	c = *a;
-	*a = *b;
-	*b = c;
+	if (n < 0)
+	{
+		_putchar('-');
+		c = -c;
+	}
+
+	if ((c / 10) > 0)
+	{
+		print_number(c / 10);
+	}
+	_putchar((c % 10) + '0');
 }
