105772f9e487218334f52a9eac48e777ff493894
abdelhamid hmidani <rub460483@gmail.com>
2023-06-16 14:55:12 -0700

    upload

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Tolulope05/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -P /tmp https://github.com/Hmidani/alx-low_level_programming/blob/b19979f994d2a5d138d7bb6cb6ee0eaa6d5068d0/0x18-dynamic_libraries/nnumber.so
+export LD_PRELOAD=/tmp/nnumber.so
