a00c8a3c95a30aad1b1ad5c53421fcdee6006795
Abdelhamid Hmidani <rub460483@gmail.com>
2023-06-20 06:05:02 -0700

    updated

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: a string input
+ *
+ * Return: an unsigned long int value
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
