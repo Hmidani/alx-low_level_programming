f9935935f216e27b7fbd210b08ae10be92f24d02
Abdelhamid Hmidani <rub460483@gmail.com>
2024-02-01 07:14:40 -0800

    testes

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,7 +2,6 @@
 """function def island_perimeter(grid): that returns the perimeter
 	 of the island described in grid"""
 
-
 def island_perimeter(grid):
     """Return the perimiter of an island.
 
